49ed9af48247fb6f53c3985cde89550b0563654b
91 <khrndnjs@gmail.com>
2022-09-23 15:23:10 +0900

    chore: 짜잘한 css수정

--- src/main/java/com/ngb/thema/controller/ThemaManageTimeFrmServlet.java
+++ src/main/java/com/ngb/thema/controller/ThemaManageTimeFrmServlet.java
@@ -1,6 +1,7 @@
 package com.ngb.thema.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.ngb.thema.model.vo.Thema;
+import com.ngb.thema.service.ThemaService;
 
 /**
  * Servlet implementation class ThemaManageTimeServlet
@@ -30,7 +34,11 @@ public class ThemaManageTimeFrmServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//1.
 		request.setCharacterEncoding("utf-8");
-		
+		//2.
+		//3.
+		ThemaService tservice = new ThemaService();
+		ArrayList<Thema> themaList = tservice.selectAllThema();
+		request.setAttribute("themaList", themaList);
 		//4.
 		RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/views/thema/themaManageTimeFrm.jsp");
 		view.forward(request, response);
