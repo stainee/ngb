edfc55a989ab77972f0cfd09b1b0ab0249be8f3f
stainee <haven2216@naver.com>
2022-09-24 11:16:09 +0900

    fix: 아이디/비밀번호 찾기 CSS 수정

--- src/main/webapp/js/searchMember.js
+++ src/main/webapp/js/searchMember.js
@@ -6,15 +6,15 @@ let regCheckMail = 0;
 $(".searchId").on("click", function() {
 	$(".first-content").removeClass("show-hide");
 	$(".second-content").addClass("show-hide");
-	$(".searchId").css("background-color", "gray");
-	$(".searchPw").css("background-color", "rgb(50,50,50)");
+	$(".searchId").css("background-color", "white").css("color","rgb(50,50,50");
+	$(".searchPw").css("background-color", "rgb(50,50,50)").css("color", "white");
 });
 
 $(".searchPw").on("click", function() {
 	$(".second-content").removeClass("show-hide");
 	$(".first-content").addClass("show-hide");
-	$(".searchPw").css("background-color", "gray");
-	$(".searchId").css("background-color", "rgb(50,50,50)");
+	$(".searchPw").css("background-color", "white").css("color", "black").css("font-weight", "700")
+	$(".searchId").css("background-color", "rgb(50,50,50)").css("color","white");
 });
 
 $(".searchId").click();
